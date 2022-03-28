276cfe91d910c2d2e272a67b5a3800ab69527c03
Lucau Rafael <72351063+luranet@users.noreply.github.com>
2022-03-28 14:57:20 +0100

    Manifest update

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 
-/*
-async function fetchData(){
-    const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data");
-    const record = await res.json();
-    document.getElementById("date").innerHTML = record.data[0].date;
-    document.getElementById("country").innerHTML = record.data[0].areaName;
-    document.getElementById("casesConfirmed").innerHTML = record.data[0].latestBy;
-    document.getElementById("deaths").innerHTML = record.data[0].deathNew;
-
-}
-fetchData();
-*/
-
 
 async function fetchData(){
     const res = await fetch("https://disease.sh/v3/covid-19/all");
